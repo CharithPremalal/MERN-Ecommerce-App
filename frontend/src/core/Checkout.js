3c5a9cb8658eebb87d910f752de62c7e87a1de9b
bryan354 <bryanchs354@gmail.com>
2020-03-20 02:05:02 +0530

    Braintree setup frontend

--- frontend/src/core/Checkout.js
+++ frontend/src/core/Checkout.js
@@ -1,13 +1,39 @@
 import React, {useState , useEffect} from 'react'
 import Layout from "./Layout";
-import {getProducts} from "./apiCore"
+import {getProducts, getBraintreeClientToken } from "./apiCore"
 import Card from "./Card";
 import {isAuthenticated} from '../auth'
 import { Link } from 'react-router-dom';
+import DropIn from "braintree-web-drop-in-react";
 
 
 
 const Checkout = ({ products }) =>{
+
+    const [data, setData] = useState({
+        success: false,
+        clientToken: null,
+        error: '',
+        instance: {},
+        address: ''
+    })
+
+    const userId = isAuthenticated() && isAuthenticated().user._id;
+    const token = isAuthenticated() && isAuthenticated().token;
+
+    const getToken = (userId, token) => {
+        getBraintreeClientToken(userId, token).then(data =>{
+            if(data.error){
+                setData({...data, error: data.error})
+            }else{
+                setData({...data, clientToken: data.clientToken});
+            }
+        })
+    }
+
+    useEffect(() => {
+        getToken(userId, token)
+    }, [])
 
 
     const getTotal = ()=>{
@@ -25,7 +51,7 @@ const Checkout = ({ products }) =>{
        return isAuthenticated()? (
 
 
-            <button className = "btn btn-success">Checkout</button>
+            <div>{showDropIn()}</div>
                 ): (
             
             <Link to= "/Signin">
@@ -38,11 +64,27 @@ const Checkout = ({ products }) =>{
                 );
     }
 
+    const showDropIn = () => (
+        <div>
+        {data.clientToken !== null && products.length > 0 ? (
+            <div>
+                <DropIn options={{
+                    authorization: data.clientToken
+                }} onInstance={instance => (data.instance = instance)}/>
+                <button className="btn btn-success">Checkout</button>
+            </div>
+        ) : null}
+        
+        </div>
+    )
+
     return(
 
         <div>
 
     <h2> Total: ${getTotal()}</h2>
+
+
 
             {showCheckout()}
         </div>
