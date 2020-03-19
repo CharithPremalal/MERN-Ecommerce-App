1d386366ebab644d08fb2badc537a6f9fe36a81e
bryan354 <bryanchs354@gmail.com>
2020-03-19 20:37:45 +0530

    create add to cart functionality

---
+++ frontend/src/core/cartHelpers.js
@@ -0,0 +1,21 @@
+// add the item to local storage
+export const addItem = (item, next) => {
+    //create empty cart 
+    let cart = []
+
+    if(typeof window !== 'undefined'){
+        if(localStorage.getItem('cart')){
+            cart = JSON.parse(localStorage.getItem('cart'))
+        }
+        cart.push({
+            ...item,
+            count: 1
+        })
+        //creating array to make no duplicate products 
+        cart = Arrya.from(new Set(cart.map((p) =>(p._id)))).map(id => {
+            return cart.find(p => p._id === id);
+        }); //new set remove duplicates 
+        localStorage.setItem('cart', JSON.stringify(cart));
+        next();
+    }
+}
