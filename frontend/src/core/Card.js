8b7e4715eae1d81703bee642a46ec9d7fef6b661
Charith Lakshitha <charithlakshitha@outlook.com>
2020-03-26 18:48:41 +0530

    Update Card.js

--- frontend/src/core/Card.js
+++ frontend/src/core/Card.js
@@ -1,110 +1,116 @@
-import React, { useState } from 'react'
-import { Link, Redirect } from 'react-router-dom'
-import ShowImage from './ShowImage'
-import moment, { updateLocale } from 'moment'
-import { addItem, updateItem, removeItem } from './cartHelpers'
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import ShowImage from "./ShowImage";
+import moment, { updateLocale } from "moment";
+import { addItem, updateItem, removeItem } from "./cartHelpers";
 
-const Card = ({product, showViewProductButton = true, showAddToCartButton = true, cartUpdate = false, showRemoveProductButton = false, setRun = f => f, run = undefined
+const Card = ({
+  product,
+  showViewProductButton = true,
+  showAddToCartButton = true,
+  cartUpdate = false,
+  showRemoveProductButton = false,
+  setRun = f => f,
+  run = undefined
   // changeCartSize
-  }) => {
-  const [redirect, setRedirect] = useState(false)
-  const [count, setCount] = useState(product.count)
+}) => {
+  const [redirect, setRedirect] = useState(false);
+  const [count, setCount] = useState(product.count);
 
   const showViewButton = showViewProductButton => {
     return (
       showViewProductButton && (
-      <Link to={`/product/${product._id}`} className='mr-2'>
-      <button className='btn btn-outline-primary mt-2 mb-2 card-btn-1'>
-        View Product
-      </button>
-      </Link>
+        <Link to={`/product/${product._id}`} className='mr-2'>
+          <button className='btn btn-outline-primary mt-2 mb-2 card-btn-1'>
+            View Product
+          </button>
+        </Link>
       )
-    )
-  }
+    );
+  };
   const addToCart = () => {
     // console.log('added')
     // addItem(product, setRedirect(true))
     addItem(product, () => {
-
-      setRedirect(true)
-    })
-  }
-
-
+      setRedirect(true);
+    });
+  };
 
   const shouldRedirect = redirect => {
     if (redirect) {
-      return <Redirect to='/cart' />
+      return <Redirect to='/cart' />;
     }
-  }
+  };
 
   const showAddToCart = showAddToCartButton => {
     return (
       showAddToCartButton && (
-      <button onClick={addToCart} className='btn btn-outline-warning mt-2 mb-2 card-btn-1  '>
-        Add to cart
-      </button>
+        <button
+          onClick={addToCart}
+          className='btn btn-outline-warning mt-2 mb-2 card-btn-1  '
+        >
+          Add to cart
+        </button>
       )
-    )
-  }
+    );
+  };
 
   const showRemoveButton = showRemoveProductButton => {
     return (
       showRemoveProductButton && (
-      <button onClick={;() => removeItem(product._id)} className='btn btn-outline-danger mt-2 mb-2 card-btn-1  '>
-        Remove Product
-      </button>
+        <button
+          onClick={() => removeItem(product._id)}
+          className='btn btn-outline-danger mt-2 mb-2 card-btn-1  '
+        >
+          Remove Product
+        </button>
       )
-    )
-  }
+    );
+  };
 
   const showStock = quantity => {
     return quantity > 0 ? (
       <span className='badge badge-primary badge-pill'>In Stock</span>
-      ) : (
+    ) : (
       <span className='badge badge-primary badge-pill'>Out of Stock</span>
-      )
-  }
+    );
+  };
 
   const handleChange = productId => event => {
-
-    setCount(event.target.value < 1 ? 1 : event.target.value)
+    setCount(event.target.value < 1 ? 1 : event.target.value);
     if (event.target.value >= 1) {
-      updateItem(productId, event.target.value)
+      updateItem(productId, event.target.value);
     }
-  }
+  };
 
   const showCartUpdateOptions = cartUpdate => {
-
-    return cartUpdate && <div>
-                           <div className='inout-group mb-3'>
-                             <div className='input-group-prepand'>
-                               <span className='input-group-text'>Adjust quantity</span>
-                             </div>
-                             <input
-                               type='number'
-                               className='form-control '
-                               value={count}
-                               onChange={handleChange(product._id)}></input>
-                           </div>
-                         </div>
-  }
+    return (
+      cartUpdate && (
+        <div>
+          <div className='inout-group mb-3'>
+            <div className='input-group-prepand'>
+              <span className='input-group-text'>Adjust quantity</span>
+            </div>
+            <input
+              type='number'
+              className='form-control '
+              value={count}
+              onChange={handleChange(product._id)}
+            ></input>
+          </div>
+        </div>
+      )
+    );
+  };
 
   return (
     <div className='card '>
-      <div className='card-header card-header-1 '>
-        {product.name}
-      </div>
+      <div className='card-header card-header-1 '>{product.name}</div>
       <div className='card-body'>
         {shouldRedirect(redirect)}
         <ShowImage item={product} url='product' />
-        <p className='card-p  mt-2'>
-          {product.description.substring(0, 100)}
-        </p>
-        <p className='card-p black-10'>
-          $
-          {product.price}
-        </p>
+        <p className='card-p  mt-2'>{product.description.substring(0, 100)}</p>
+        <p className='card-p black-10'>${product.price}</p>
         <p className='black-9'>
           Category:
           {product.category && product.category.name}
@@ -121,7 +127,7 @@ const Card = ({product, showViewProductButton = true, showAddToCartButton = true
         {showCartUpdateOptions(cartUpdate)}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
