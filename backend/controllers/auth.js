a7ab64fbad2306afe95f0d6e768390a68ed446d6
Minu Helaka Jayakody <minu.jayakody.mj@gmail.com>
2020-03-31 02:01:11 +0530

    conflict resolve

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -48,12 +48,18 @@ exports.signout = (req, res) => {
   res.json({ message: 'Signout success ' })
 }
 
+
+
+
 exports.requireSignin = expressJwt({
   secret: process.env.JWT_SECRET,
   userProperty: 'auth'
 })
 
-exports.isAuth = (req, res, next) => {
+
+
+exports.isAuth = (req,res, next) => {
+
 
   let user = req.profile && req.auth && req.profile._id == req.auth._id
 
